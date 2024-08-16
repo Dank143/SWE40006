99238f7efcc2245be9b10fbf43e2b117043a32ec
Dang Nguyen <131951723+Dank143@users.noreply.github.com>
2024-08-16 22:35:46 +1000

    Add files via upload

---
+++ Task 1/ExampleDesktop/GenderLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenderLibrary
+{
+    public class GenderHandler
+    {
+        public string GetGender(string gender)
+        {
+            return gender;
+        }
+    }
+}
