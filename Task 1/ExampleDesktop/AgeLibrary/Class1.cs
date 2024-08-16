ffad8bcec6d455ef12e522e73bc932a89beae832
Dang Nguyen <131951723+Dank143@users.noreply.github.com>
2024-08-16 22:34:09 +1000

    Add files via upload

---
+++ Task 1/ExampleDesktop/AgeLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeLibrary
+{
+    public class AgeHandler
+    {
+        public int GetAge(int age)
+        {
+            return age;
+        }
+    }
+}
