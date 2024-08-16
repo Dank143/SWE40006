b504f3de7e6d8bdd9d9e3f825b1542003d5fd3fa
Dang Nguyen <131951723+Dank143@users.noreply.github.com>
2024-08-16 22:36:18 +1000

    Add files via upload

---
+++ Task 1/ExampleDesktop/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Display "Hello, World!" when the form loads
+            MessageBox.Show("Hello, World!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            MessageBox.Show($"Nice to meet you, {name}!");
+        }
+    }
+}
